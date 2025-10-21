c373c5028b5931fe1cf005b921999c2d17fc505d
Raelag2 <klimovmotty@gmail.com>
2025-10-21 11:44:22 +0300

    Йоу

--- src/lab04/text_report.py
+++ src/lab04/text_report.py
@@ -1,6 +1,17 @@
-with open('input.txt', 'r', encoding='utf-8') as file:
-    for line in file:
-        print(line.strip())
+import sys
+import os
+from pathlib import Path
+
+project_root = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, project_root)
+
+from src.lib.text import normalize, tokenize, count_freq, top_n
+
+full_text = Path('././data/lab04/input.txt')
+with open(full_text, 'r', encoding='utf-8') as f:
+    content = f.read()
     
+string = top_n(tokenize(full_text), 5)
+print(string)
 
 
